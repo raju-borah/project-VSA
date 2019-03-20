e325497d8e8a095faa6db349e023e707226289d6
Nabajyoti <sirnaba@gmail.com>
2019-03-20 10:07:57 +0530

    done

---
+++ js/main.js
@@ -0,0 +1,49 @@
+//display serach option
+document.querySelector('.statusBar__item--icon').addEventListener('click',function()
+{
+    document.querySelector('.search').style.display = 'block';
+    console.log("done");
+
+}
+);
+
+
+document.querySelector('.statusBar__close').addEventListener('click',function()
+{
+    document.querySelector('.search').style.display = 'none';
+    console.log("done");
+
+}
+);
+
+
+//sticky status bar
+let navbar = document.querySelector('.navbar');
+
+let sticky = navbar.offsetTop;
+
+console.log(sticky);
+
+function position(){
+
+    if(window.pageYOffset >= sticky)
+    {
+
+        navbar.classList.add("fixed");
+        document.querySelector('.statusBar__item--icon').style.display = 'none';
+        document.querySelector('.navbar').style.backgroundImage ="linear-gradient(to right,rgba(46, 19, 199, 0.800),rgba(18, 207, 240, 0.400))";
+
+    }
+    else{
+
+        navbar.classList.remove("fixed");
+        document.querySelector('.statusBar__item--icon').style.display = 'block';
+        document.querySelector('.navbar').style.backgroundImage ="linear-gradient(to right,rgba(46, 19, 199, 0.800),rgba(18, 207, 240, 0.400))";
+    }
+}
+
+window.onscroll = function()
+{
+    position();
+}
+
