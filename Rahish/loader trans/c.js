2462e8d56ef7e4550f95e90451de02b9c4f8f161
raju-borah <raju42fun@gmail.com>
2019-04-16 19:24:37 +0530

    desgins corrected

---
+++ Rahish/loader trans/c.js
@@ -0,0 +1,18 @@
+// play on load for gallery view
+setTimeout(transition, 1000);
+
+$('.js-trigger-transition').on('click', function(e) {
+  e.preventDefault();
+  transition();
+});
+
+function transition() {
+  var tl = new TimelineMax();
+  
+  tl.to(CSSRulePlugin.getRule('body:before'), 0.2, {cssRule: {top: '50%' }, ease: Power2.easeOut}, 'close')
+  .to(CSSRulePlugin.getRule('body:after'), 0.2, {cssRule: {bottom: '50%' }, ease: Power2.easeOut}, 'close')
+  .to($('.loader'), 0.2, {opacity: 1})
+  .to(CSSRulePlugin.getRule('body:before'), 0.2, {cssRule: {top: '0%' }, ease: Power2.easeOut}, '+=1.5', 'open')
+  .to(CSSRulePlugin.getRule('body:after'), 0.2, {cssRule: {bottom: '0%' }, ease: Power2.easeOut}, '-=0.2', 'open')
+  .to($('.loader'), 0.2, {opacity: 0}, '-=0.2');
+}
