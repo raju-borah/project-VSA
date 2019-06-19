9387e53e89bb5999da98617a8540714dd37a7479
Nabajyoti <sirnaba@gmail.com>
2019-06-20 02:08:11 +0530

    profile page responsive work left

---
+++ website/js/editform.js
@@ -0,0 +1,20 @@
+//btn to open the form
+const editBtn=document.querySelector('.editform');
+
+////name of the editform
+const editform=document.querySelector('.editpanel');
+
+// btn to close the form
+const closebtn=document.querySelector('.close');
+
+
+
+// When the user clicks the button, open the modal 
+editBtn.addEventListener('click',() =>{
+  editform.style.display = "block";
+})
+
+//when the user click the close btn form is closed
+closebtn.addEventListener('click',() =>{
+ editform.style.display = "none";
+})
