6b56a8720429b640f7966a939f1b06615ba69c88
Nabajyoti <sirnaba@gmail.com>
2019-06-20 12:31:31 +0530

    responsive work on profile page done

--- website/js/editform.js
+++ website/js/editform.js
@@ -1,12 +1,11 @@
 //btn to open the form
-const editBtn=document.querySelector('.editform');
+const editBtn=document.querySelector('.editbtn');
 
 ////name of the editform
 const editform=document.querySelector('.editpanel');
 
 // btn to close the form
 const closebtn=document.querySelector('.close');
-
 
 
 // When the user clicks the button, open the modal 
