8fd18bcc01b463373f502cf1644905ce2635abbe
vikizax <44132243+vikizax@users.noreply.github.com>
2019-06-23 18:21:00 +0530

    Pwa Added

--- vsa-vue/src/firebase/init.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase'
-
-var config = {
-  // add firebase project config here
-};
-const firebaseApp = firebase.initializeApp(config);
-let functions = firebaseApp.functions()
-export default firebaseApp.firestore()
-export { functions }
