c5e7621ac4e695009b28671dccbbb5d018a628db
Nabajyoti <sirnaba@gmail.com>
2019-06-23 22:31:07 +0530

    notification panel done .. button to open it is not decided

---
+++ website/js/notify.js
@@ -0,0 +1,19 @@
+//btn to open the form
+const notifyBtn=document.querySelector('.editbtn');
+
+////name of the editform
+const notificationpanel=document.querySelector('.notify');
+
+// btn to close the form
+const closebtn=document.querySelector('.notify_panel--close');
+
+
+// When the user clicks the button, open the modal 
+notifyBtn.addEventListener('click',() =>{
+    notificationpanel.style.display = "block";
+})
+
+//when the user click the close btn form is closed
+closebtn.addEventListener('click',() =>{
+    notificationpanel.style.display = "none";
+})
